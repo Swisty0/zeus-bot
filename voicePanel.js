bae9d8ae77923ad8add58df7006e65aad1fbc3b2
Swisty0 <laxed0481@gmail.com>
2026-09-21 18:31:38 +0300

    Update voicePanel.js

--- voicePanel.js
+++ voicePanel.js
@@ -82,19 +82,17 @@ module.exports = {
             if (message.content === '!ses' && message.member.permissions.has('Administrator')) {
                 const embed = new EmbedBuilder()
                     .setColor(0x5865F2)
-                    .setTitle('7/24 Ses Bağlantı Paneli')
                     .setDescription(
-                        '<:site:1551605548590309387> **ZEUS — Bot Ses Yönetimi**\n\n' +
+                        '# <:site:1551605548590309387> **ZEUS — Bot Ses Yönetimi**\n\n' +
                         '<a:dclogo:1551605543716782261> **Sistem Durumu:** `Hazır / Çevrimiçi`\n' +
                         '<a:strike:1551605532018868275> **Bot Limiti:** `0/10`'+
-                        '──────────────────────────────────────────────────────────\n\n' +
+                        '──────────────────────────────────────────────────────\n\n' +
                         '# <:classadam:1551605484178378822> Bot Yönetimi Nedir?\n\n'+
                         'Standart Discord bot tokenlerini ses kanallarına **7/24 kesintisiz** bağlar. Aktif bot listeni görebilir, durum varlıklarını değiştirebilir ve bağlantıları kesebilirsin.\n\n' +
                         '───────────────────────────────────────────────────────────\n\n' +
                         '# <:olympyx_ay:1551605489056354337> Seçenekler?\n\n'+
-                        '<a:snowflake:1551605446203408384>` **Bot Sese Kur** — Tekli veya toplu bot tokenlerini ses kanalına sokar\n' +
-                        '<a:snowflake:1551605446203408384>` **Bağlantıyı Kes** — Ses bağlantılarını güvenle sonlandırır'+
-                        '<a:coding:1551605537446297803> Tokenlerin **şifrelenerek** saklanır.'
+                        '> <:plus:1551605515375607888> **Bot Sese Kur** — Tekli veya toplu bot tokenlerini ses kanalına sokar\n' +
+                        '> <:silme:1551605463395868782> **Bağlantıyı Kes** — Ses bağlantılarını güvenle sonlandırır' +
                     )
                     .setFooter({ text: 'Zeus Bot • Encrypted Voice System' });
 
