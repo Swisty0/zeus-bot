7cab3a3a9ec6d6a5c9580db6c9a969595f6247ff
Swisty0 <laxed0481@gmail.com>
2026-09-21 17:16:32 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000 
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
-
 require('dotenv').config();
 const { Client, GatewayIntentBits, Partials, Collection } = require('discord.js');
 const fs = require('fs');
